1dca1df7ca023b31e3a390d9bdd86d9ff3cc4d5e
Didier Bonnet <didier.dbo@gmail.com>
2026-02-17 09:36:44 +0100

    add authenticated decorator to control function execution based on user validity

--- decorators.py
+++ decorators.py
@@ -32,3 +32,26 @@ def long_time():
         i * 5
 
 long_time()
+
+
+# Create an @authenticated decorator that only allows the function to run if user1 has 'valid' set to True:
+user1 = {
+    'name': 'Sorna',
+    'valid': False #changing this will either run or not run the message_friends function.
+}
+
+def authenticated(fn):
+    def wrapper(*args, **kwargs):
+        if args[0]['valid']:
+            return fn(*args, **kwargs)
+        else:
+            print("User is not authenticated")
+    return wrapper
+
+@authenticated
+def message_friends(user):
+    print('message has been sent')
+
+message_friends(user1)
+user1['valid'] = True
+message_friends(user1)
